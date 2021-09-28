7e51b09b4481fc04f2833f7505c99f34708fbd02
jatin_nb <jatin.goel@newsbytesapp.com>
2021-09-28 23:56:35 +0530

    Updated example of Abstract Factory

--- Abstract-Factory/example_1.py
+++ Abstract-Factory/example_1.py
@@ -1,13 +1,34 @@
 # Python Code for object
-# oriented concepts without
-# using the Abstract factory
-# method in class
+# oriented concepts using
+# the abstract factory
+# design pattern
+
+import random
+
+class Course_At_GFG:
+
+	""" GeeksforGeeks portal for courses """
+
+	def __init__(self, courses_factory = None):
+		"""course factory is out abstract factory"""
+
+		self.course_factory = courses_factory
+
+	def show_course(self):
+
+		"""creates and shows courses using the abstract factory"""
+
+		course = self.course_factory()
+
+		print(f'We have a course named {course}')
+		print(f'its price is {course.Fee()}')
+
 
 class DSA:
 
-	""" Class for Data Structure and Algorithms """
+	"""Class for Data Structure and Algorithms"""
 
-	def price(self):
+	def Fee(self):
 		return 11000
 
 	def __str__(self):
@@ -17,7 +38,7 @@ class STL:
 
 	"""Class for Standard Template Library"""
 
-	def price(self):
+	def Fee(self):
 		return 8000
 
 	def __str__(self):
@@ -27,19 +48,22 @@ class SDE:
 
 	"""Class for Software Development Engineer"""
 
-	def price(self):
+	def Fee(self):
 		return 15000
 
 	def __str__(self):
 		return 'SDE'
 
-# main method
+def random_course():
+
+	"""A random class for choosing the course"""
+
+	return random.choice([SDE, STL, DSA])()
+
+
 if __name__ == "__main__":
 
-	sde = SDE() # object for SDE class
-	dsa = DSA() # object for DSA class
-	stl = STL() # object for STL class
+	course = Course_At_GFG(random_course)
 
-	print(f'Name of the course is {sde} and its price is {sde.price()}')
-	print(f'Name of the course is {dsa} and its price is {dsa.price()}')
-	print(f'Name of the course is {stl} and its price is {stl.price()}')
+	for i in range(5):
+		course.show_course()
